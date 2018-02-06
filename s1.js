8075fc7854d9f1b59acf3c6085a93b153c3b2fc2
Hadesnow <snowbloom1991@gmail.com>
2018-02-06 20:38:01 +0100

    Update s1.js

--- s1.js
+++ s1.js
@@ -1,31 +1,38 @@
-var cov=0;
-var cnv;
+var imgs=[];
+var num;
+var count=0;
 function setup() {
-  cnv=createCanvas(windowWidth,windowHeight);
-
+  var cnv = createCanvas(windowWidth,windowHeight);
+  cnv.position(0,0);
+  background(0);
+  cnv.style('z-index',-10);
+  for(var i=0;i<13;i++){
+    imgs[i]=createImg('images/second'+i+'.svg');
+    imgs[i].position(0,0);
+    imgs[i].style('opacity',0);
+    imgs[i].size(height*3/4,height*3/4);
+    imgs[i].position(width/2-height*3/8,height/8);
+    
+  }
   textAlign(CENTER);
-  textFont('monospace');
+  textFont('Share Tech Mono');
 }
 
 function draw() {
-  background('#222');
-  textSize(width/24);
+  background('#000');
+  textSize(18);
   fill('#1cc7d0');
-  text('Move your mouse',width/2,height/2);
-  // if(frameCount<256){
-  //   cov=frameCount;
-  // }else{cov=255}
-  fill(0,cov);
-  rect(0,0,width,height);
-  if(cov>255){
-    cov=frameCount;
-    window.location.href="s2.html"; 
-  }
+   text('click mouse to find the circle',width/2,height/2);
+
 }
 
 function windowResized(){
   resizeCanvas(windowWidth,windowHeight);
 }
-function mouseMoved()  {
-  cov++;
+function mousePressed(){   
+    if(count>=12){
+       window.location.href="s3.html"; 
+    }
+    imgs[round(count)].style('opacity',1);
+    count+=0.5;
 }
