6256e69a9e7cc8b3bea2d464f2fab393eb44023e
Hadesnow <snowbloom1991@gmail.com>
2018-02-06 20:38:41 +0100

    Update s2.js

--- s2.js
+++ s2.js
@@ -1,40 +1,55 @@
-var imgs=[];
-var num;
-var count=0;
+var timer=350;
+var colors=[];
+
 function setup() {
-  var cnv = createCanvas(windowWidth,windowHeight);
-  cnv.position(0,0);
-  background('#222');
-  cnv.style('z-index',-10);
-  for(var i=0;i<13;i++){
-    imgs[i]=createImg('images/second'+i+'.svg');
-    imgs[i].position(0,0);
-    imgs[i].style('opacity',0);
-    imgs[i].size(height*3/4,height*3/4);
-    imgs[i].position(width/2-height*3/8,height/8);
-    
-  }
+  createCanvas(windowWidth,windowHeight);
+  angleMode(DEGREES);
   textAlign(CENTER);
-  textFont('monospace');
+  textSize(18);
+  background(0);
+  push();
+  fill(255);
+  ellipse(windowWidth/2,windowHeight/2,windowHeight*3/4);
+  pop();
+  textFont('Share Tech Mono');
+  var w = 20;
+       var int=setInterval(function(){ 
+            var fills 
+            =color(30,0,10+w/3);
+            colors.push(fills);
+            fill(fills);
+            arc(0,0, height*3/4, height*3/4, w-20, w);
+            w+=20;     
+            if(w>360+360){
+              int=window.clearInterval(int); 
+            }
+      }, 120);
 }
 
 function draw() {
-  textSize(22);
-  fill(255);
-   text('Click to finish the circle',width/2,height/2);
-      if(count>12){
-    window.location.href="s3.html"; 
+    noStroke();
+    ellipse(windowWidth);
+    translate(width/2,height/2);
+    if(frameCount>=timer ){
+      background(28,100,208);
+      rotate(frameCount-timer);
+      for(var i=0;i<colors.length;i++){
+        fill(colors[i]);
+        arc(0,0, height*3/4*frameCount/timer, height*3/4*frameCount/timer, i*20, (i+1)*20);
+      }    
     }
-
+    if(frameCount>=240){
+       background(2,frameCount-240);
+       
+       fill(28,100,208,frameCount-320);
+       ellipse(0,0,height*3/4);
+       
+       if(frameCount>=575){
+         window.location.href="s4.html"; 
+       }
+    }
+    
 }
-
 function windowResized(){
   resizeCanvas(windowWidth,windowHeight);
-}
-function mousePressed(){   
-    if(count>=12){
-       window.location.href="s3.html"; 
-    }
-    imgs[round(count)].style('opacity',1);
-    count+=0.6;
 }
